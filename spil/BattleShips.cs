48d76eb14e30898f56842038b6a0fd5f30138922
LucaPortale <luca0421@edu.eal.dk>
2017-10-05 12:18:44 +0200

    message hej

--- spil/BattleShips.cs
+++ spil/BattleShips.cs
@@ -1,10 +1,41 @@
-﻿namespace spil
+﻿using System;
+namespace spil
 {
     internal class BattleShips
     {
         public string GetBattleShipsGameBoardView()
         {
-            return "Koden for Battleships gameboardview";
+
         }
+
+        public string GetPlayerName()
+        {
+            Console.WriteLine("Indtast dit navn");
+            string getPlayerName = Console.ReadLine();
+            return getPlayerName;
+        }
+
+        public void PlaceShip(string shipName, int shipLength)
+        {
+            Console.WriteLine("Indtast X og Y koordinater for stævnen på " + shipName);
+            string firstPlacement = Console.ReadLine();
+            int xKoordinat;
+            int yKoordinat;
+            if (firstPlacement.Length == 2 && int.TryParse(firstPlacement, out int s))
+            {
+                xKoordinat = (int) firstPlacement[0];
+                yKoordinat = (int) firstPlacement[1];
+
+            }
+            else
+            {
+                Console.WriteLine("Ugyldigt");
+            }
+        }
+
+        public void ValidatePlacement(int xKoordinat, int yKoordinat)
+            {
+                
+            }
     }
 }
---
+++ spil/BattleShipsPlayer.cs
@@ -0,0 +1,22 @@
+﻿namespace spil
+{
+    internal class BattleShipsPlayer
+    {
+        public string[] hangar = new string[5];
+        public string[] slagSkib1 = new string[4];
+        public string[] slagSkib2 = new string[4];
+        public string[] destroyer1 = new string[3];
+        public string[] destroyer2 = new string[3];
+        public string[] ubaad = new string[3];
+        public string[] patruljebåd1 = new string[2];
+        public string[] patruljebåd2 = new string[2];
+        public string[] patruljebåd3 = new string[2];
+        public string name;
+        public string Name 
+            { 
+            get { return this.name; }
+            set { this.name = value; }
+            }
+
+    }
+}
